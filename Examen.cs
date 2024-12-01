21aa0e7f02d5e639a95c3cabc5c40058e9882884
arik-0 <arikayelin@gmail.com>
2024-12-01 16:37:22 -0300

    primer version

---
+++ Examen.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace proyecto_final_PED
+{
+    public class Examen
+    {
+        public int ExamenId { get; set; }
+        public DateTime Fecha { get; set; }
+        public string Asignatura { get; set; }
+        public List<int> ListaPreguntas { get; set; }
+
+        public Examen(int examenId, DateTime fecha, string asignatura, List<int> listaPreguntas)
+        {
+            ExamenId = examenId;
+            Fecha = fecha;
+            Asignatura = asignatura;
+            ListaPreguntas = listaPreguntas;
+        }
+
+        public override string ToString()
+        {
+            return $"Examen ID: {ExamenId}\nFecha: {Fecha}\nAsignatura: {Asignatura}\nPreguntas: {string.Join(", ", ListaPreguntas)}";
+        }
+    }
+}
---
+++ Pregunta.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.Xml;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace proyecto_final_PED
+{
+    public class Pregunta
+    {
+        /*PreguntaId, txtPregunta, Lista de respuestas, respuestaCorrecta, asignatura, unidad, subunidad.*/
+        public int PreguntaId{ get; set; }
+        public string TxtPregunta { get; set; }
+        public List<string> Opciones { get; set; }
+        public int RespuestaCorrecta { get; set; }
+        public string Asignatura { get; set; }
+        public int Unidad { get; set; }
+        public int SubUnidad { get; set; }
+
+        public Pregunta(int preguntaId, string textoPregunta, List<string> opciones, int respuestaCorrecta, string asignatura, int unidad, int subunidad)
+        {
+            if (opciones.Count > 4)
+                throw new ArgumentException("La pregunta no puede tener más de 4 opciones.");
+
+            PreguntaId = preguntaId;
+            TxtPregunta = textoPregunta;
+            Opciones = opciones;
+            RespuestaCorrecta = respuestaCorrecta;
+            Asignatura = asignatura;
+            Unidad = unidad;
+            SubUnidad = subunidad;
+        }
+
+        public bool EsRespuestaCorrecta(int indiceRespuesta)
+        {
+            return RespuestaCorrecta == indiceRespuesta;
+        }
+
+        public override string ToString()
+        {
+            string opcionesTexto = "";
+            for (int i = 0; i < Opciones.Count; i++)
+            {
+                opcionesTexto += $"{i + 1}. {Opciones[i]}\n";
+            }
+
+            return $"ID: {PreguntaId}\nPregunta: {TxtPregunta}\nOpciones:\n{opcionesTexto}Asignatura: {Asignatura}\nUnidad: {Unidad}, Subunidad: {SubUnidad}";
+        }
+    }
+}
